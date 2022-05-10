02fdb4b78aa8f45ffa01d86d5217d96b475e9ebd
FaiyaazHawk <faiyaaz.haque90@gmail.com>
2022-05-09 23:11:37 -0400

    password validation complete. project complete

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ const mismatch = document.getElementById('mismatch')
 
 function validatePassword(){
     if(password.value != confirm_password.value || (password.value === '' && confirm_password.value === '')) {
-    confirm_password.setCustomValidity("Error: Passwords don't match. Please try again.");
+    
     password.style.borderColor = "red";
     confirm_password.style.borderColor = "red";
     mismatch.textContent = "*Passwords do not match";
-    mismatch.classList.add('error');
+    
     } else {
-    confirm_password.setCustomValidity('');
     mismatch.textContent = "";
     }
     if(password.value === confirm_password.value) {
-        password.style.borderColor = "#3cd33c";
-        confirm_password.style.borderColor = "#3cd33c";
+        password.style.borderColor = "green";
+        confirm_password.style.borderColor = "green";
         
     }
+    
 }
+
+function passwordError() {
+    password.style.borderColor = "red";
+    confirm_password.style.borderColor = "red";
+    mismatch.textContent = "*Passwords do not match";
+    
+}
+
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
 
+window.onload = passwordError;
 
-
-window.onload = redPassword;
-
-function redPassword() {
-    password.style.borderColor = "#C13838";
-    confirm_password.style.borderColor = "#C13838";
-    mismatch.textContent = "*Passwords do not match";
-    mismatch.classList.add("error");
-}
